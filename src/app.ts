5f2f67bef2ef1e50347eac480d57cf6c7a2ac42a
Jyotiranjan Sahoo <jsahoo.p@gmail.com>
2024-04-23 21:04:05 +0530

    connect to mongo db

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,25 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
 import { handleError } from './utils/error';
 import { bookRouter } from './routes/bookRoutes';
 
 const app: express.Application = express();
+
+const uri = 'mongodb://localhost:27017';
+const client = new MongoClient(uri);
+
+async function connectToMongo() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB', error);
+  } finally {
+    await client.close();
+  }
+}
+
+connectToMongo();
 
 app.use(handleError);
 
